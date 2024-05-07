b933fc45118b84309d1a3e7503fdaeaa3310ebf2
Cloutzie <accounts@shwalker.org>
2024-05-07 16:57:16 -0700

    feat: fetch player name URIs

---
+++ src/player-paths.py
@@ -0,0 +1,30 @@
+"""
+Generate player URIs as a dict.
+
+import player-paths
+
+dict = player_paths.get_dict(letters)
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+
+
+def get_dict(self, letters):
+    """
+    Takes a string of letters to get player names of.
+
+    Returns a dict of player names like 'letter' : 'list of names'
+    """
+
+    _dict = {}
+
+    for _ in (letters):
+        _url = f"https://www.basketball-reference.com/players/{_}/"
+        _html = (requests.get(_url)).text
+        _parser_obj = BeautifulSoup(_html, "html.parser")
+
+        _dict[f"{_}"] = [(h.find("a", href=True)).get("href") for s in _parser_obj.findAll("tr") for h in s.findAll("strong")]
+
+    return _dict
