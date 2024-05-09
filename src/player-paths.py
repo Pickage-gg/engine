86b1ddcd3cad971c65895df7eefe55407eeff8d4
Carson Batchelor <carsonb1723@gmail.com>
2024-05-08 20:53:50 -0500

    feat: error handling

--- src/player-paths.py
+++ src/player-paths.py
@@ -14,7 +14,6 @@ import json
 import os
 
 
-
 def get_dict(letters):
     """
     Takes a string of letters to get player names of.
@@ -24,14 +23,19 @@ def get_dict(letters):
 
     _dict = {}
 
-    for _ in (letters):
+    for _ in letters:
         _url = f"https://www.basketball-reference.com/players/{_}/"
         _html = (requests.get(_url)).text
         _parser_obj = BeautifulSoup(_html, "html.parser")
 
-        _dict[f"{_}"] = [(h.find("a", href=True)).get("href") for s in _parser_obj.find_all("tr") for h in s.find_all("strong")]
+        _dict[f"{_}"] = [
+            (h.find("a", href=True)).get("href")
+            for s in _parser_obj.find_all("tr")
+            for h in s.find_all("strong")
+        ]
 
     return _dict
+
 
 def dict_to_json():
     """
@@ -39,18 +43,15 @@ def dict_to_json():
     """
 
     dict = get_dict("abcdefghijklmnopqrstuvwxyz")
-    urijson_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data', "URI.json"))
-    if dict:
+    urijson_path = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "data", "URI.json")
+    )
+    if all(map(bool, dict.values())):
         try:
             with open(urijson_path, "w") as fp:
                 json.dump(dict, fp)
 
         except:
-            """probably put error handling here"""
-            print("dict made but no json dumo")
+            return None
     else:
-        print("no dick")
-
-
-dic = get_dict("abcdefghijklmnopqrstuvwxyz")
-print(dic['a'])
+        return None
